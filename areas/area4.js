1ec336a3bd9a184eaa765e59bacaf25b937730d2
Nicola Saljoughi <nicolasaljoughi@gmail.com>
2021-06-08 14:55:53 +0200

    Create area4.js

---
+++ areas/area4.js
@@ -0,0 +1,35 @@
+
+var fourMarker = {
+		"meta": {
+			"createdBy": "Me",
+			"createdAt": "2021-06-08"
+		},
+		"trackingBackend": "artoolkit",
+		"subMarkersControls": [
+			{
+				"parameters": {
+					"type": "barcode",
+					"value": "1"
+				},
+				"poseMatrix": [
+					1,
+					0,
+					0,
+					0,
+					0,
+					1,
+					0,
+					0,
+					0,
+					0,
+					1,
+					0,
+					0,
+					0,
+					0,
+					1
+				]
+			}
+		]
+	}	
+	localStorage.setItem("fourMarkerFile", JSON.stringify(fourMarker));
